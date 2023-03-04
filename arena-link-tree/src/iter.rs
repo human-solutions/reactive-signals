8abea364ab5451e56f04fb7047b6ba96230f28cd
hakesson <henrik@akesson.mobi>
2023-03-04 09:34:55 +0100

    Add _skip + renaming to make clearer

--- arena-link-tree/src/iter.rs
+++ arena-link-tree/src/iter.rs
@@ -4,16 +4,24 @@ pub struct DepthFirstIter<'a, T> {
     tree: &'a Tree<T>,
     start: NodeId,
     /// A node who's children have all been visited
-    current: Option<NodeId>,
+    next: Option<NodeId>,
+    /// Node that should not be visited (including it's children)
+    skip: Option<NodeId>,
 }
 
 impl<'a, T> DepthFirstIter<'a, T> {
     pub(crate) fn new(tree: &'a Tree<T>, start: NodeId) -> Self {
+        let next = Some(drill_down(tree, start));
+        DEBUG.then(|| println!("Start: {start:?}, Next: {next:?}"));
         Self {
             tree,
             start,
-            current: Some(drill_down(tree, start)),
+            next,
+            skip: None,
         }
+    }
+    pub fn skip(&mut self, node: NodeId) {
+        self.skip = Some(node);
     }
 }
 
@@ -29,41 +37,42 @@ const DEBUG: bool = false;
 fn next<T>(
     tree: &Tree<T>,
     start: NodeId,
-    mut current: Option<NodeId>,
+    mut next: Option<NodeId>,
+    _skip: Option<NodeId>,
 ) -> (Option<NodeId>, Option<NodeId>) {
-    let Some(next) = current else {
+    let Some(current) = next else {
         DEBUG.then(|| println!("[STOP]. Current is none"));
         return (None, None);
     };
 
-    DEBUG.then(|| print!("[{next:?}] "));
+    DEBUG.then(|| print!("[{current:?}] "));
 
-    if next == start {
-        DEBUG.then(|| println!("Stop next. Same as start: {current:?}"));
+    if current == start {
+        DEBUG.then(|| println!("Stop next. Current same as start: {start:?}"));
         return (None, Some(start));
-    } else if let Some(node) = tree.nodes[next.index()].prev_sibling {
+    } else if let Some(node) = tree.nodes[current.index()].prev_sibling {
         // we found a previous sibling, let's start at that sibling's deepest child
-        current = Some(drill_down(tree, node));
-        DEBUG.then(|| println!("Next is prev siblings deepest child: {current:?}"));
-    } else if let Some(parent) = tree.nodes[next.index()].parent {
+        next = Some(drill_down(tree, node));
+        DEBUG.then(|| println!("Next is prev siblings deepest child: {next:?}"));
+    } else if let Some(parent) = tree.nodes[current.index()].parent {
         // there's no previous sibling so we have to go up knowing
         // that all the children of the parent have been visited
-        current = Some(parent);
-        DEBUG.then(|| println!("Next is parent: {current:?}"));
+        next = Some(parent);
+        DEBUG.then(|| println!("Next is parent: {next:?}"));
     } else {
         panic!("BUG: iteration ended up in a state that should be impossible");
     }
 
-    return (current, Some(next));
+    return (next, Some(current));
 }
 
 impl<'a, T> Iterator for DepthFirstIter<'a, T> {
     type Item = NodeId;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let (current, next) = next(self.tree, self.start, self.current);
-        self.current = current;
-        return next;
+        let (next, current) = next(self.tree, self.start, self.next, None);
+        self.next = next;
+        return current;
     }
 }
 
@@ -71,23 +80,29 @@ pub struct MutDepthFirstIter<'a, T> {
     tree: &'a mut Tree<T>,
     start: NodeId,
     /// A node who's children have all been visited
-    current: Option<NodeId>,
+    next: Option<NodeId>,
+    skip: Option<NodeId>,
 }
 
 impl<'a, T> MutDepthFirstIter<'a, T> {
     pub(crate) fn new(tree: &'a mut Tree<T>, start: NodeId) -> Self {
-        let current = Some(drill_down(tree, start));
-        DEBUG.then(|| println!("Start: {start:?}, Current: {current:?}"));
+        let next = Some(drill_down(tree, start));
+        DEBUG.then(|| println!("Start: {start:?}, Next: {next:?}"));
         Self {
             tree,
             start,
-            current,
+            next,
+            skip: None,
         }
     }
+    pub fn skip(&mut self, node: NodeId) {
+        self.skip = Some(node);
+    }
+
     pub fn for_each(&mut self, f: impl Fn(&mut Tree<T>, NodeId)) {
-        while let (current, Some(next)) = next(self.tree, self.start, self.current) {
-            f(self.tree, next);
-            self.current = current;
+        while let (next, Some(current)) = next(self.tree, self.start, self.next, self.skip) {
+            self.next = next;
+            f(self.tree, current);
         }
     }
 }
