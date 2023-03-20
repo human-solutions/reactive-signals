243ab1df407ad7ce3c5c7d265f474e9c9c89892a
hakesson <henrik@akesson.mobi>
2023-03-20 17:45:25 +0100

    Impl UnsafeCell SingleRuntime

--- reactor/src/runtimes/mod.rs
+++ reactor/src/runtimes/mod.rs
@@ -2,17 +2,29 @@
 
 mod inner;
 mod pool;
-mod single;
 
 pub(crate) use inner::RuntimeInner;
 
 #[cfg(any(test, feature = "profile"))]
-pub use self::{pool::PoolRuntimeId, single::SingleRuntimeId};
+pub use self::pool::PoolRuntimeId;
 
 use crate::Scope;
 
 pub use pool::RuntimePool;
+
+#[cfg(not(feature = "use-unsafe"))]
+mod single;
+#[cfg(not(feature = "use-unsafe"))]
 pub use single::SingleRuntime;
+#[cfg(all(not(feature = "use-unsafe"), any(test, feature = "profile")))]
+pub use single::SingleRuntimeId;
+
+#[cfg(feature = "use-unsafe")]
+mod single_unsafe;
+#[cfg(feature = "use-unsafe")]
+pub use single_unsafe::SingleRuntime;
+#[cfg(all(feature = "use-unsafe", any(test, feature = "profile")))]
+pub use single_unsafe::SingleRuntimeId;
 
 pub trait Runtime: Default + Copy + 'static {
     fn with_ref<F, T>(&self, f: F) -> T
